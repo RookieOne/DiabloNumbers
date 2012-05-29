01d23ce58c90cb8f1665a57c40a068dd6fdaeb2f
Jonathan Birkholz <rookieone@gmail.com>
2012-05-29 15:22:54 -0500

    dr from armor topic

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,9 @@
+class TopicsController < ApplicationController
+
+  def index
+  end
+  
+  def dr_from_armor
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ DiabloNumbers::Application.routes.draw do
 
   match "references", :to => "pages#references"
   match "definitions", :to => "pages#definitions"
+
+  resources :topics, :only => [:index] do
+    collection do
+      get :dr_from_armor
+    end
+  end
+  
   root :to => 'home#index'
 
 end
