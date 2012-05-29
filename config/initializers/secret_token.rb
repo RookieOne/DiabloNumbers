62a3b7e9c7856fc0a8982b1a9d52ac50a6aef07e
Jonathan Birkholz <rookieone@gmail.com>
2012-05-29 13:01:49 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DiabloNumbers::Application.config.secret_token = '0cb8ee0f0e831071018c56aed8ff4aa0a980181110910b79450f05999ae30780d32b80ba59128b19c7a9e234337e800cbc4aa85a5981019af23f10128f30655e'
